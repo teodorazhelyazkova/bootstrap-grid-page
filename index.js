1c7944c71de3cd396e83ee6846e2cc55771851cd
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2022-10-13 17:38:09 +0300

    add pagination logic and styling

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* NEWSLETTER BUTTON */
+
 const form = document.querySelector("form");
 
 form.addEventListener("submit", (event) => {
@@ -7,6 +9,8 @@ form.addEventListener("submit", (event) => {
 
   form.classList.add("was-validated");
 });
+
+/* CAROUSEL SLIDER */
 
 let items = document.querySelectorAll(".carousel .carousel-item");
 
@@ -23,3 +27,74 @@ items.forEach((el) => {
     next = next.nextElementSibling;
   }
 });
+
+/* PAGINATION */
+const paginationNumbers = document.getElementById("pagination-numbers");
+const paginatedList = document.getElementById("paginated-list");
+const listItems = paginatedList.querySelectorAll("li");
+
+const paginationLimit = 8;
+const pageCount = Math.ceil(listItems.length / paginationLimit);
+let currentPage;
+
+const appendPageNumber = (index) => {
+  const pageNumber = document.createElement("li");
+  const pageLink = document.createElement("a");
+
+  pageNumber.className = "page-item border border-0";
+  pageLink.className = "pagination-number page-link bg-white text-muted";
+  pageLink.innerHTML = index;
+  pageLink.setAttribute("page-index", index);
+  pageLink.setAttribute("aria-label", "Page " + index);
+
+  pageNumber.appendChild(pageLink);
+  paginationNumbers.appendChild(pageNumber);
+};
+
+const getPaginationNumbers = () => {
+  for (let i = 1; i <= pageCount; i++) {
+    appendPageNumber(i);
+  }
+};
+
+const setCurrentPage = (pageNum) => {
+  currentPage = pageNum;
+
+  handleActivePageNumber();
+
+  const prevRange = (pageNum - 1) * paginationLimit;
+  const currRange = pageNum * paginationLimit;
+
+  listItems.forEach((item, index) => {
+    item.setAttribute("hidden", "hidden");
+    if (index >= prevRange && index < currRange) {
+      item.removeAttribute("hidden");
+    }
+  });
+};
+
+window.addEventListener("load", () => {
+  getPaginationNumbers();
+  setCurrentPage(1);
+
+  document.querySelectorAll(".pagination-number").forEach((pageLink) => {
+    const pageIndex = Number(pageLink.getAttribute("page-index"));
+
+    if (pageIndex) {
+      pageLink.addEventListener("click", () => {
+        setCurrentPage(pageIndex);
+      });
+    }
+  });
+});
+
+const handleActivePageNumber = () => {
+  document.querySelectorAll(".pagination-number").forEach((pageLink) => {
+    pageLink.classList.remove("active");
+
+    const pageIndex = Number(pageLink.getAttribute("page-index"));
+    if (pageIndex == currentPage) {
+      pageLink.classList.add("active");
+    }
+  });
+};
